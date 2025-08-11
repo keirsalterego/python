f1da1e1ac18cea545bad4132c25b66f4ce4fe2c5
keir <darksalterego99@gail.com>
2025-08-11 09:16:43 +0530

    initialzed

---
+++ 11-08/q1.py
@@ -0,0 +1,9 @@
+# ###### 11-08 ######
+# This script prints a series of messages to the console.
+
+print("keir")
+print("Hello, World!")
+print("This is a test script.")
+print("Goodbye!")
+print("End of script.")
+
